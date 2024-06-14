6e43efdf20145cfa840712832d23ab53bcca1969
hoanghuy <hoanghuy7284022@gmail.com>
2024-06-15 02:24:07 +0700

    update decrease request + virtual message

--- app/src/main/java/com/example/myandroidproject/admin/fragment/AdminChatFragment.java
+++ app/src/main/java/com/example/myandroidproject/admin/fragment/AdminChatFragment.java
@@ -39,7 +39,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -49,57 +51,118 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class AdminChatFragment extends Fragment {
-    int ID_USER = 1;
+    int ID_USER;
     int customerId;
-    final Set<Integer> admins = new HashSet<>();
+
+    String tempOwnerName = null;
+
+    List<Message> messages = new ArrayList<>();
+    MessageAdapter adapter = new MessageAdapter(messages);
+
     ImageView chatExitBtnAdmin;
     TextView sendBtnAdmin;
     RecyclerView recyclerViewAdmin;
     TextInputEditText messageInputAdmin;
     RequestQueue queue;
-    List<Message> messages = new ArrayList<>();
-    MessageAdapter adapter = new MessageAdapter(messages);
-    Timer timer = new Timer();
+    Timer fetchMessages;
 
     public AdminChatFragment() {
         // Required empty public constructor
     }
 
-
+    @Nullable
     @Override
-    public void onStart() {
-        super.onStart();
-        ID_USER = SharedPreferencesUtils.getInt(SharedPreferencesUtils.STATE_USER_ID, requireContext());
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat_admin, container, false);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        customerId = getArguments().getInt("customerId");
+        if (getArguments() != null)
+            customerId = getArguments().getInt("customerId");
+        ID_USER = SharedPreferencesUtils.getInt(SharedPreferencesUtils.STATE_USER_ID, requireContext());
         super.onCreate(savedInstanceState);
-        queue = Volley.newRequestQueue(getActivity());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        queue = Volley.newRequestQueue(requireContext());
+        fetchMessages = new Timer();
+        fetchMessages.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                JSONObject jsonBody = new JSONObject();
+                try {
+                    jsonBody.put("id", ID_USER);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest rq = new JsonObjectRequest(Request.Method.POST, URL_READ_MESSAGE, jsonBody, resp -> {
+                    try {
+                        JSONArray res = resp.getJSONArray("data");
+                        List<Message> temp = new ArrayList<>();
+                        for (int i = 0; i < res.length(); i++) {
+                            JSONObject jsonObject = res.getJSONObject(i);
+                            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH);
+                            temp.add(new Message(jsonObject.getInt("from"), jsonObject.getInt("to"), jsonObject.getString("fromFirstName"), jsonObject.getString("toFirstName"), jsonObject.getString("content"), formatter.parse(jsonObject.getString("createAt")), jsonObject.getInt("from") == ID_USER ? true : false));
+                        }
+                            //Filter message relate with customerId current
+                            temp.removeIf(message ->{
+                                //Get first name (for virtual message on first request)
+                                boolean isCustomer = message.getFrom() == customerId || message.getTo() == customerId;
+                                if (isCustomer && tempOwnerName == null)
+                                    if (message.getFrom() == ID_USER){
+                                        tempOwnerName = message.getFromFirstName();
+                                    }else{
+                                        tempOwnerName = message.getToFirstName();
+                                    }
+
+                                return !isCustomer;
+                            });
+
+                            if (temp.size() > messages.size()) {
+                                messages.clear();
+                                messages.addAll(temp);
+                                messages.sort(Message::sortOldToNew);
+                                adapter.notifyDataSetChanged(); //Only change SOMETHING SHOULD BE CHANGEEEEEEEEEEEEEE!
+                                recyclerViewAdmin.scrollToPosition(messages.size() - 1);
+                            }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, Throwable::printStackTrace);
+                queue.add(rq);
+                System.out.println("fetch at admin");
+            }
+        }, 100, 10000);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         chatExitBtnAdmin = view.findViewById(R.id.chatExitBtnAdmin);
         recyclerViewAdmin = view.findViewById(R.id.admin_chat_recycleview);
         sendBtnAdmin = view.findViewById(R.id.sendBtnAdmin);
         messageInputAdmin = view.findViewById(R.id.messageInputAdmin);
+
         recyclerViewAdmin.setAdapter(adapter);
         recyclerViewAdmin.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         messageInputAdmin.setOnKeyListener((v, keyCode, event) -> {
-            if (event.getAction() == KeyEvent.ACTION_DOWN){
-                if (keyCode == KeyEvent.KEYCODE_ENTER){
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
                     Editable e = messageInputAdmin.getText();
-                    if (e == null){
+                    if (e == null) {
                         Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
                         return false;
                     }
                     String msg = e.toString().trim();
 
-                    if (msg.isEmpty() || msg.isBlank()){
+                    if (msg.isEmpty() || msg.isBlank()) {
                         Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
                         return false;
                     }
@@ -111,149 +174,67 @@ public class AdminChatFragment extends Fragment {
             return false;
         });
 
-        messageInputAdmin.setOnKeyListener((v, keyCode, event) -> {
-            if (event.getAction() == KeyEvent.ACTION_DOWN){
-                if (keyCode == KeyEvent.KEYCODE_ENTER){
-                    Editable e = messageInputAdmin.getText();
-                    if (e == null){
-                        Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
-                        return false;
-                    }
-                    String msg = e.toString().trim();
-
-                    if (msg.isEmpty() || msg.isBlank()){
-                        Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
-                        return false;
-                    }
-
-                    sendMessage(msg);
-                    e.clear();
-                }
+        sendBtnAdmin.setOnClickListener(v -> {
+            Editable e = messageInputAdmin.getText();
+            if (e == null) {
+                Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
+                return;
             }
-            return false;
+            String msg = e.toString().trim();
+
+            if (msg.isEmpty() || msg.isBlank()) {
+                Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
+                return;
+            }
+            sendMessage(msg);
+            e.clear();
         });
 
         chatExitBtnAdmin.setOnClickListener(v -> {
             NavController navController = Navigation.findNavController(view);
             navController.navigate(R.id.fragment_admin_chat_storage);
         });
-
-
-        sendBtnAdmin.setOnClickListener(v->{
-            Editable e = messageInputAdmin.getText();
-            if (e == null){
-                Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
-                return;
-            }
-            String msg = e.toString().trim();
-
-            if (msg.isEmpty() || msg.isBlank()){
-                Toast.makeText(getActivity(), "Invalid input", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            sendMessage(msg);
-            e.clear();
-        });
-
-
     }
 
-    private void sendMessage(String msg){
-        if (admins.size() <= 0){
-            JsonObjectRequest rq = new JsonObjectRequest(Request.Method.GET, URL_GET_ADMINS_MESSAGE, null, resp->{
-                try {
-                    JSONArray res = resp.getJSONArray("data");
-                    for (int i=0; i<res.length(); i++){
-                        admins.add(res.getInt(i));
-                    }
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }, err->{err.printStackTrace();});
-            queue.add(rq);
-        }
+    private void sendMessage(String msg) {
+        //Create a virtual send message
+        Message virtualMessage = new Message(ID_USER, -1, tempOwnerName, null, msg, Date.from(Instant.now()), true);
+        virtualMessage.isVirtualMessage = true;
+        messages.add(virtualMessage);
+        adapter.notifyItemInserted(messages.size());
+        recyclerViewAdmin.scrollToPosition(messages.size() - 1);
 
-
-            JSONObject jsonBody = new JSONObject();
-            try {
-                jsonBody.put("content", msg);
-                jsonBody.put("from", ID_USER);
-                jsonBody.put("to", customerId);
-            }catch (Exception e){
-                Toast.makeText(getActivity(), "Gửi tin thất bại", Toast.LENGTH_LONG).show();
-            }
-
-            JsonObjectRequest rq1 = new JsonObjectRequest(Request.Method.POST, URL_SEND_MESSAGE, jsonBody, resp1->{}, err->{err.printStackTrace();});
-            queue.add(rq1);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         JSONObject jsonBody = new JSONObject();
         try {
-            jsonBody.put("id", ID_USER);
-        }catch (Exception e){
-            e.printStackTrace();
+            jsonBody.put("content", msg);
+            jsonBody.put("from", ID_USER);
+            jsonBody.put("to", customerId);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Gửi tin thất bại", Toast.LENGTH_LONG).show();
         }
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                JsonObjectRequest rq = new JsonObjectRequest(Request.Method.POST, URL_READ_MESSAGE, jsonBody, resp->{
-
-                    try {
-                        JSONArray res = resp.getJSONArray("data");
-                        List<Message> temp = new ArrayList<>();
-                        for (int i=0; i<res.length(); i++){
-                            JSONObject jsonObject = res.getJSONObject(i);
-                            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH);
-                            temp.add(new Message(jsonObject.getInt("from"), jsonObject.getInt("to"), jsonObject.getString("fromFirstName"), jsonObject.getString("toFirstName"), jsonObject.getString("content"), formatter.parse(jsonObject.getString("createAt")), jsonObject.getInt("from")==ID_USER?true:false));
-                            temp.sort(Message::sortOldToNew);
-
-                            if (temp.size() > messages.size()){
-                                messages.clear();
-
-                                if (!temp.isEmpty()){
-                                        for (Message m: temp){
-                                            if (m.getFrom() == customerId || m.getTo() == customerId){
-                                                messages.add(m);
-                                            }
-                                        }
-                                    messages.sort(Message::sortOldToNew);
-                                }
-
-                                adapter.notifyDataSetChanged();
-                            }
-
-                            recyclerViewAdmin.scrollToPosition(messages.size()-1);
-                        }
-
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-
-                }, err->{
-
-                });
-
-                queue.add(rq);
-            }
-        }, 100, 2000);
-
-
-        return inflater.inflate(R.layout.fragment_chat_admin, container, false);
+        JsonObjectRequest rq1 = new JsonObjectRequest(Request.Method.POST, URL_SEND_MESSAGE, jsonBody, resp1 -> {
+            messages.remove(virtualMessage);
+        }, Throwable::printStackTrace);
+        queue.add(rq1);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (queue != null){
+        if (queue != null)
             queue.cancelAll(this);
-        }
-        timer.cancel();
+
+        if (fetchMessages != null)
+            fetchMessages.cancel();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (queue != null)
+            queue.cancelAll(this);
+
+        if (fetchMessages != null)
+            fetchMessages.cancel();
     }
 }
